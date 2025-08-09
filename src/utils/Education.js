3237286f35308b37102ffb16faca9d5b33a99f11
saffr3n <saffr3n@gmail.com>
2025-08-09 18:59:49 +0300

    feat: add Education and Practice utility classes

---
+++ src/utils/Education.js
@@ -0,0 +1,8 @@
+export default class Education {
+  constructor(school = '', study = '', start = '', end = '') {
+    this.school = school;
+    this.study = study;
+    this.start = start;
+    this.end = end;
+  }
+}
---
+++ src/utils/Practice.js
@@ -0,0 +1,15 @@
+export default class Practice {
+  constructor(
+    company = '',
+    position = '',
+    description = '',
+    start = '',
+    end = '',
+  ) {
+    this.company = company;
+    this.position = position;
+    this.description = description;
+    this.start = start;
+    this.end = end;
+  }
+}
