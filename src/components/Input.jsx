02cafe091b271a9eab41550970b24b33ba282397
saffr3n <saffr3n@gmail.com>
2025-08-09 18:57:31 +0300

    feat: add Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,22 @@
+export default function Input({
+  type,
+  name,
+  label,
+  required,
+  value,
+  onChange,
+}) {
+  return (
+    <label htmlFor={name}>
+      {label}
+      <input
+        type={type}
+        name={name}
+        id={name}
+        required={required}
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
