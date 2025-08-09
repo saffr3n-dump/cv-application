9c90672dd42c42c631829d9f2342719a70c98d35
saffr3n <saffr3n@gmail.com>
2025-08-09 19:05:15 +0300

    feat: add initial App state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { useState } from 'react';
+import Education from './utils/Education';
+import Practice from './utils/Practice';
+
 export default function App() {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    education: [new Education()],
+    practice: [new Practice()],
+  });
+
   return <h1>CV Application</h1>;
 }
