486b25d39d7d050dd66adf0e2f284d242d64ea37
saffr3n <saffr3n@gmail.com>
2025-08-10 17:38:22 +0300

    feat: render Form or Info conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Form from './components/Form';
+import Info from './components/Info';
 import Education from './utils/Education';
 import Practice from './utils/Practice';
 
 export default function App() {
+  const [editMode, setEditMode] = useState(true);
   const [data, setData] = useState({
     name: '',
     email: '',
@@ -15,7 +17,11 @@ export default function App() {
   return (
     <>
       <h1>CV Application</h1>
-      <Form data={data} setData={setData} />
+      {editMode ? (
+        <Form data={data} setData={setData} setEditMode={setEditMode} />
+      ) : (
+        <Info data={data} setEditMode={setEditMode} />
+      )}
     </>
   );
 }
